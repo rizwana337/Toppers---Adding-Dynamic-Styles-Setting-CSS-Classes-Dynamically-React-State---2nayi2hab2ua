e002c43e5103768481f4df58d4208627b1b8a407
rizwana337 <120568097+rizwana337@users.noreply.github.com>
2023-04-07 21:48:21 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React, {Component, useState} from "react";
 import '../styles/App.css';
 
 class App extends Component {
+	
 	constructor(props)
 	{
 		super(props);
@@ -38,33 +39,35 @@ class App extends Component {
 			],
 		}
 	};
-    render() {
+	
+   
+	 render() {
     	return(
-    		<div id="main">
+		
+       	<div id="main">
     			{
-    				//Correct the percentage conditional for correct output
-    				this.state.list.map(function(item,index){
-				   		if(item.percent >75)
+			
+    				// Correct the percentage conditional for correct output
+    				this.state.list.map(function(item,index)
+					{
+						//  const [bgpink, changeBackground] = useState("");
+				   		if(item.percent > 75)
 				   		{
 				   			return(
-				   				//Conditional statement in the className of below div to give callName="bg-pink"
-				   				//if the percent >= 90 else className = "" 
-				   				<div key={index}
-                                                                     className={item.percent >= 90 ? "bg-pink" : ""}>
+				   				// Conditional statement in the className of below div to give callName="bg-pink"
+				   				// if the percent >= 90 else className = "" 
+				   				<div key={index} className={item.percent>90 ? "bg-pink": ""}>
 					   				<div className="name">
-					   					 //Write code to display name
-										{item.name}
+					   					 {/* //Write code to display name */}
+										 {item.name}
 					   				</div>
 					   				<div className="percent">
-					   					 //Write code to display percentage upto 2 decimal places
-										{parseFloat(item.percent).toFixed(2)}
+					   					 {/* // Write code to display percentage upto 2 decimal places  */}
+										 {item.percent.toFixed(2)}
 					   				</div>
 				   				</div>
 				   				);
-	     } else {
-            return null;
-          }
-				   		
+				   		}
 			       })
 			    }
     		</div>
@@ -74,4 +77,80 @@ class App extends Component {
 
 
 export default App;
+// import React, {Component, useState} from "react";
+// import '../styles/App.css';
+
+// class App extends Component {
+// 	constructor(props)
+// 	{
+// 		super(props);
+// 		this.state = {
+// 			list : [
+// 				{
+// 					name : "naman",
+// 					percent : 60.0000
+// 				},
+// 				{
+// 					name : "ritesh",
+// 					percent : 99.9431
+// 				},
+// 				{
+// 					name : "hitesh",
+// 					percent : 65.3365
+// 				},
+// 				{
+// 					name : "shreya",
+// 					percent : 80.6400
+// 				},
+// 				{
+// 					name : "ankur",
+// 					percent : 78.8970
+// 				},
+// 				{
+// 					name : "vanshu",
+// 					percent : 78.9876
+// 				},
+// 				{
+// 					name : "krupal",
+// 					percent : 85.3214
+// 				},
+// 			],
+// 		}
+// 	};
+//     render() {
+//     	return(
+//     		<div id="main">
+//     			{
+//     				//Correct the percentage conditional for correct output
+//     				this.state.list.map(function(item,index){
+// 				   		if(item.percent >75)
+// 				   		{
+// 				   			return(
+// 				   				//Conditional statement in the className of below div to give callName="bg-pink"
+// 				   				//if the percent >= 90 else className = "" 
+// 				   				<div key={index}
+//                                                                      className={item.percent >= 90 ? "bg-pink" : ""}>
+// 					   				<div className="name">
+// 					   					 //Write code to display name
+// 										{item.name}
+// 					   				</div>
+// 					   				<div className="percent">
+// 					   					 //Write code to display percentage upto 2 decimal places
+// 										{parseFloat(item.percent).toFixed(2)}
+// 					   				</div>
+// 				   				</div>
+// 				   				);
+// 	     } else {
+//             return null;
+//           }
+				   		
+// 			       })
+// 			    }
+//     		</div>
+//     		);
+//     }
+// }
+
+
+// export default App;
 
